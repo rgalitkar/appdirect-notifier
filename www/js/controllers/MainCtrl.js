84fa65ff8e38114759e7ef9487719728ad3b0834
rakesh galitkar <rakesh.galitkar@appdirect.com>
2017-02-09 19:46:13 +0530

    Added main page UI

--- www/js/controllers/MainCtrl.js
+++ www/js/controllers/MainCtrl.js
@@ -1,3 +1,24 @@
 controllerModule.controller('MainCtrl', function($scope, $stateParams) {
-  $scope.msg = "Your POST content goes here";
+
+  $scope.notify = {
+    isCabAvailed: false,
+    showCabOption:false,
+    listOption: null,
+    availableOptions: [
+      {id: '', name: 'Select Option'},
+      {id: '1', name: 'Coming Late'},
+      {id: '2', name: 'Leaving Early'},
+      {id: '3', name: 'Work From Home'},
+      {id: '4', name: 'Out Of Office'},
+      {id: '5', name: 'PTO (Pre Approved)'},
+      {id: '6', name: 'Sick Leave'},
+      {id: '7', name: 'Working Late'},
+    ]
+  };
+
+  $scope.$watch('notify.listOption', function(n){
+      $scope.notify.showCabOption = (n && n == '7');
+  });
+
+
 });
