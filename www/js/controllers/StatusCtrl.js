2cbb5111c6103c79cfe82892e54572437078a263
Prashant Dukare <prashant.dukare@appdirect.com>
2017-02-13 16:24:41 +0530

    Status cntroller added

--- www/js/controllers/StatusCtrl.js
+++ www/js/controllers/StatusCtrl.js
@@ -1,26 +1,24 @@
 var app = angular.module('app-notifier')
 .controller('StatusCtrl', function($scope, $stateParams) {
   $scope.msg = "Your Status content goes here";
-  $scope.teamAlertsDataToday= [
+  $scope.individualAlerts= [
     {
-      name: 'Rakesh',
-      status: '[LATE] By : 2:00 P.M.',
-      description: 'Doctor\'s appointment ..'
+      name: 'Today',
+      data: [
+        {
+          status: '[LATE] By : 2:00 P.M.',
+          description: 'Doctor\'s appointment ..'
+        }
+      ]
     },
     {
-      name: 'Prashant',
-      status: '[OOO] 12:00 To 2:00 P.M.',
-      description: 'Stepping out for lunch'
-    },
-    {
-      name: 'Rohit',
-      status: '[WFH]',
-      description: 'Need to take care some personal stuff'
-    },
-    {
-      name: 'Nitin',
-      status: '[Leaving-Early] By : 4:00 P.M',
-      description: 'Room hunt...'
+      name: 'Upcoming',
+      data: [
+        {
+          status: '[WFH] on 17/02/2017',
+          description: 'Need to take care some personal stuff'
+        }
+      ]
     }
   ];
 });
