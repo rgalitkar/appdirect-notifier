1e675fe35a870ad290a4dedfea950fa6c1e55643
rakesh galitkar <rakesh.galitkar@appdirect.com>
2017-02-13 18:08:55 +0530

    Status

--- www/js/controllers/StatusCtrl.js
+++ www/js/controllers/StatusCtrl.js
@@ -1,33 +1,45 @@
 var app = angular.module('app-notifier')
 .controller('StatusCtrl', function($scope, $stateParams) {
   $scope.msg = "Your Status content goes here";
-  $scope.individualAlerts= [
-    {
-      name: 'Today',
-      data: [
+  $scope.individualAlerts= [];
+
+
+  $scope.searchData = function($e){
+
+    if($e.keyCode == 13) {
+      $scope.individualAlerts = [
         {
-          status: '[LATE] By : 2:00 P.M.',
-          description: 'Doctor\'s appointment ..'
-        }
-      ]
-    },
-    {
-      name: 'Tomorrow',
-      data: [
+          name: 'Today',
+          data: [
+            {
+              status: '[LATE] By : 2:00 P.M.',
+              description: 'Doctor\'s appointment ..'
+            }
+          ]
+        },
         {
-          status: '[WFH]',
-          description: 'Need to take care some personal stuff'
-        }
-      ]
-    },
-    {
-      name: 'Upcoming',
-      data: [
+          name: 'Tomorrow',
+          data: [
+            {
+              status: '[WFH]',
+              description: 'Need to take care some personal stuff'
+            }
+          ]
+        },
         {
-          status: '[WFH] on 17/02/2017',
-          description: 'Need to take care some personal stuff'
+          name: 'Upcoming',
+          data: [
+            {
+              status: '[WFH] on 17/02/2017',
+              description: 'Need to take care some personal stuff'
+            }
+          ]
         }
-      ]
+      ];
+
     }
-  ];
+
+
+  };
+
 });
